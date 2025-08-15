fc623b02ad57635d1cb002317d467e3b1a6563aa
naeyra <noeliacontea@gmail.com>
2025-08-15 17:34:13 +0200

    Programo ejercicio2.py

---
+++ python/ejercicio2.py
@@ -0,0 +1,9 @@
+#Ejercicio 2 será utilizar las variables que te da el usuario:
+#Pedimos las variables:
+nombre = input("¿Cómo te llamas? ")
+edad = input("¿Cuántos años tienes? ")
+ciudad= input("¿Dónde naciste? ")
+
+#Imprimimos el mensaje:
+print(f"Hola {nombre}, tienes {edad} años y eres de {ciudad}.")
+
